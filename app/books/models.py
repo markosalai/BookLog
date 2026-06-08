04d2d3b4b5893f7049bf24da944f2963e40b648d
markosalai <marko.salai@fsre.sum.ba>
2026-06-08 11:05:05 +0200

    dodan ekvivalent BookModel i ReviewModel

--- app/books/models.py
+++ app/books/models.py
@@ -4,6 +4,25 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 
 # Create your models here.
+
+class KnjigaManager(models.Manager):
+    def find_all(self):
+        return self.all()
+
+    def find_by_user(self, korisnik_id):
+        return self.filter(korisnik_id=korisnik_id)
+
+    def find_by_id(self, id):
+        return self.filter(id=id).first()
+    
+    def search(self, query):
+        return self.filter(
+            models.Q(naslov__icontains=query) |
+            models.Q(autor__icontains=query) |
+            models.Q(zanr__icontains=query)
+        )
+
+
 class Knjiga(models.Model):
     naslov = models.CharField(max_length=255)
     autor = models.CharField(max_length=255)
@@ -17,11 +36,40 @@ class Knjiga(models.Model):
         on_delete=models.CASCADE
     )
 
+    objects = KnjigaManager()
+
     def __repr__(self):
         return f"Knjiga({self.naslov}, {self.isbn}, {self.autor}, {self.zanr}, {self.opis}, {self.godina_izdanja}, {self.korisnik})"
 
     def __str__(self):
-        return self.naslov
+        return f"{self.naslov} - {self.autor}"
+    
+    @classmethod
+    def create(cls, naslov, autor, isbn, korisnik, **kwargs):
+        return cls.objects.create(
+            naslov=naslov,
+            autor=autor,
+            isbn=isbn,
+            korisnik=korisnik,
+            **kwargs
+        )
+    
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
+        return self
+
+    def delete_knjiga(self):
+        self.delete()
+
+
+class RecenzijaManager(models.Manager):
+    def find_by_book(self, knjiga_id):
+        return self.filter(knjiga_id=knjiga_id)
+
+    def find_by_user(self, korisnik_id):
+        return self.filter(korisnik_id=korisnik_id)
     
 class Recenzija(models.Model):
     tekst = models.TextField(blank=True, null=True)
@@ -36,6 +84,27 @@ class Recenzija(models.Model):
         on_delete=models.CASCADE
     )
     knjiga = models.ForeignKey(Knjiga, on_delete=models.CASCADE)
+
+    objects = RecenzijaManager()
+
+    @classmethod
+    def create(cls, tekst, ocjena, korisnik, knjiga):
+        return cls.objects.create(
+            tekst=tekst,
+            ocjena=ocjena,
+            korisnik=korisnik,
+            knjiga=knjiga,
+        )
+    
+    def update(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
+        return self
+    
+    def toggle_visibility(self):
+        self.vidljiva = not self.vidljiva
+        self.save()
 
     class Meta:
         unique_together = [('korisnik', 'knjiga')]
--- app/books/views.py
+++ app/books/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Knjiga
 
 # Create your views here.
+
+def book_list(request):
+    books = Knjiga.objects.find_all()
+    return render(request, "books/list.html", {'books': books})
